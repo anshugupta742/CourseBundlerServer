131acd1297eba5141e8fde8f229860ecd7e4e29b
anshugupta742 <anshugupta4742@gmail.com>
2024-07-20 00:53:25 +0530

    Changes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import ErrorMiddleware from "./middlewares/Error.js";
 import cookieParser from "cookie-parser";
 import payment from "./routes/paymentRoutes.js";
 import other from "./routes/otherRoutes.js";
+import cors from "cors";
 
 config({
     path: "./config/config.env",
@@ -19,6 +20,13 @@ app.use(express.urlencoded({
 })
 );
 app.use(cookieParser());
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL,
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+    })
+);
 
 app.use("/api/v1", course);
 app.use("/api/v1", user);
@@ -26,5 +34,11 @@ app.use("/api/v1", payment);
 app.use("/api/v1", other);
 
 export default app;
+
+app.get("/", (req,res) =>
+    res.send(
+        `<h1>Site is Working. Click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
+    )
+);
 
 app.use(ErrorMiddleware);
